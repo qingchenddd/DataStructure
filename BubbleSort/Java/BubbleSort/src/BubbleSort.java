4d83f29c086b88dca763258cff8941144d88e698
qingchenddd <1624015463@qq.com>
2022-07-24 22:04:53 +0800

    冒泡Java

--- BubbleSort/Java/BubbleSort/src/BubbleSort.java
+++ BubbleSort/Java/BubbleSort/src/BubbleSort.java
@@ -5,5 +5,27 @@
 
 public class BubbleSort
 {
-    
+    public static void main(String[] args)
+    {
+        int[] data = {11,22,99,88,55,44,77};
+        //基本数据类型传值，对象传地址
+        bubbleSort(data);
+        for (int datum : data)
+            System.out.println(datum);
+    }
+
+    public static void bubbleSort(int[] data)
+    {
+        int temp;    // 定义临时数
+        // 共进行N遍操作
+        for (int i = 0; i < data.length; i++)     // 共遍历N遍
+            for (int j = 0; j < data.length - i - 1; j++)     // 让每一个数与后一个数比较，若后一个大/小则交换
+                if (data[j] > data[j + 1])
+                {
+                    // 交换数据
+                    temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+    }
 }
--- BubbleSort/Python/BubbleSort.py
+++ BubbleSort/Python/BubbleSort.py
@@ -3,3 +3,20 @@
 # @Author : 李兆堃
 # @File : BubbleSort.py
 # @Software : PyCharm
+
+def main():
+   data = [11, 22, 1, 99, 58, 64, 78, 156, 178]
+   for i in range(len(data)-1):
+       index = i
+       for j in range(i+1, len(data)):
+           if data[j] < data[index]:
+               index = j
+       temp = data[index]
+       data[index] = data[i]
+       data[i] = temp
+   print(data)
+   pass
+
+
+if __name__ == "__main__":
+   	main()
